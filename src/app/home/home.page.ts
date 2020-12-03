3d7c78719ea52f77ae956ef29169ed6272ae1377
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2020-12-02 20:17:21 -0600

    Se muestran las peliculas de todos los usuarios en la página principal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeliculasService } from '../api/peliculas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  peliculas = [];/* [
+    {
+      titulo: "Ready Player One",
+      anio: 2018,
+      director: "Steven Spielberg",
+      imagen: "https://ae01.alicdn.com/kf/HTB1le4dluuSBuNjy1Xcq6AYjFXap/Decoraci-n-de-pared-de-lona-personalizada-p-ster-de-pel-cula-Ready-Player-One-Wall.jpg"
+    },
+    {
+      titulo: "TRON Legacy",
+      anio: 2010,
+      director: "Joseph Kosinski",
+      imagen: "https://images-na.ssl-images-amazon.com/images/I/91NeDaqrX3L._SL1500_.jpg"
+    }
+  ]; */
+  
+  constructor(private peliculasService: PeliculasService) {}
 
-  constructor() {}
+  ngOnInit() {
+    this.peliculasService.peliculas.subscribe(peliculas => {
+      this.peliculas = peliculas;
+    });
+    // this.peliculasService.getPeliculas();
+  }
 
 }
